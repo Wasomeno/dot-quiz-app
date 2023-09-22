28dbac4e7a8e9150b405fec1f925bd40c8c33c81
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-22 15:02:13 +0700

    refactor: change position

--- src/components/user-menu-dialog.tsx
+++ src/components/user-menu-dialog.tsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export function UserMenuDialog() {
   const { user, logout } = useAuth0();
   return (
-    <div className="absolute -bottom-[7.75rem] -left-56  z-20 flex w-64 flex-1  flex-col rounded-lg border bg-white shadow-md">
+    <div className="absolute -bottom-28 -left-56 z-20  flex w-64 flex-1 flex-col  rounded-lg border bg-white shadow-md ">
       <div className="flex items-center gap-4 border-b px-6 py-3">
         <img
           className="h-8 w-8 overflow-hidden rounded-full"
