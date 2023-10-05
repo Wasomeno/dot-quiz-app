30409940dd38d37c56b49a7ddb88166145f9eef1
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-05 12:56:07 +0700

    refactor: implement useQuizStorage custom hook

--- src/components/session-exist-alert-modal.tsx
+++ src/components/session-exist-alert-modal.tsx
@@ -1,12 +1,8 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+import { useQuizStorage } from "../hooks/useQuizStorage";
 
 export function SessionExistAlertModal() {
-  const authSession = useAuth0();
-  const userQuizData = JSON.parse(
-    localStorage.getItem(authSession.user?.email as string) as string,
-  );
-
+  const { data, removeQuizStorageData } = useQuizStorage();
   return (
     <>
       <div className="absolute left-0 top-0 z-10 h-screen w-screen bg-slate-800 bg-opacity-50 backdrop-blur-sm" />
@@ -19,32 +15,30 @@ export function SessionExistAlertModal() {
             You still have an active Quiz Session
           </span>
           <div className="space-y-2 text-center">
-            <div className="font-medium lg:text-lg">
-              {userQuizData.questions[0].category}
-            </div>
+            <div className="font-medium lg:text-lg">{data.category?.name}</div>
             <div className="text-sm font-medium lg:text-base">
-              {userQuizData.result.answered !== 10
-                ? `0${(userQuizData.timer / 60).toString()[0]} : ${
-                    (userQuizData.timer % 60).toString().length < 2
-                      ? `0${(userQuizData.timer % 60).toString()}`
-                      : (userQuizData.timer % 60).toString()
+              {data.answers?.length !== 10
+                ? `0${((data?.timer as number) / 60).toString()[0]} : ${
+                    ((data?.timer as number) % 60).toString().length < 2
+                      ? `0${((data?.timer as number) % 60).toString()}`
+                      : ((data?.timer as number) % 60).toString()
                   } Left`
                 : "Finished"}
             </div>
             <div className="text-sm font-medium lg:text-base">
-              {userQuizData.result.answered} / 10 Answered
+              {data?.answers?.length} / 10 Answered
             </div>
           </div>
         </div>
         <div className="flex items-center gap-4">
-          <Link to={`/quiz/${userQuizData.categoryId}`}>
+          <Link to={`/quiz/${data?.category?.id}`}>
             <button className="w-40 rounded-md bg-green-700 py-2 text-sm font-medium text-white transition duration-200 lg:hover:bg-green-700/90">
               Continue
             </button>
           </Link>
           <button
             onClick={() => {
-              localStorage.removeItem(authSession.user?.email as string);
+              removeQuizStorageData();
               location.reload();
             }}
             className="w-40 rounded-md bg-red-700 py-2 text-sm font-medium text-white transition duration-200 lg:hover:bg-red-700/90"
